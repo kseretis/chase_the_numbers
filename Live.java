a38cf26099b6035134c2e8c1a44c7fe0bb0bcc7e
kseretis <serkleanthis@gmail.com>
2022-06-08 21:32:08 +0300

    New Live class added

---
+++ Live.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Live here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Live extends Actor
+{
+    /**
+     * Act - do whatever the Live wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
